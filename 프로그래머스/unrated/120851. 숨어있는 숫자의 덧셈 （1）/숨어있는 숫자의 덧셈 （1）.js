2b742133ec033350d6b79930335d8027f690f3c5
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2023-10-31 18:32:30 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var arr = [...my_string]
+    var sum = 0;
+    arr.map(item=> {
+        if(Number(item)) sum+=Number(item)
+    })
+    
+    return sum;
+}
+
+/*
+문자열 배열 아이템으로 다 나누고 그 중 숫자만 구해서 값 더하기 , 숫자인것만
+*/
