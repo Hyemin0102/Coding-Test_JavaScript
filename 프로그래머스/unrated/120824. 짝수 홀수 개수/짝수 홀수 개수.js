ff02fa33277c83a05021604477ba5bb055ad99a3
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2023-10-31 15:58:21 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,4 +1,12 @@
 function solution(num_list) {
+    return [
+        num_list.filter(item => (item % 2) === 0).length,
+        num_list.filter(item => (item % 2) === 1).length,
+    ]
+}
+
+
+/*function solution(num_list) {
     let num = 0; 
     num_list.map(item=>{
         if(item % 2 === 0) num++;
@@ -6,9 +14,8 @@ function solution(num_list) {
     let num2 = num_list.length - num; 
     let answer = [num, num2];
     return answer;
-}
+}*/
 
 /*
 2로 나눴을 때 0인 것의 개수, 0이 아닌 것의 개수
-
 */
