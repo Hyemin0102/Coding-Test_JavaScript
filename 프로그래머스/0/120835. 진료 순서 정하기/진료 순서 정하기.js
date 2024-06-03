3ea52a2180b0079627fb748f415e6d6ec28f9c52
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2024-06-03 19:51:43 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -0,0 +1,23 @@
+function solution(emergency) {
+    //배열 내림차순 -> 새 배열 생성
+    let bb = [...emergency].sort(function(a,b){
+      return b-a
+    });
+    
+    let result = [];
+    //내림차순한 배열로 인덱스 부여
+    bb.forEach((item,index)=>{
+      result.push([item,index+1])
+    });
+
+    let test = [];
+    emergency.forEach(item => {
+        //이중배열 중 원래 배열의 0번째 인덱스와 맞는값 찾아서 1번째 인덱스 출력
+      let dd = result.find(arr => arr[0] === item );
+      if (dd) {
+        test.push(dd[1])
+      }
+    })
+    
+    return test;
+}
