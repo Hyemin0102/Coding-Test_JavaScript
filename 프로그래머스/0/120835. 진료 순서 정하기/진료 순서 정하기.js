c25c8686c85f7e032624ba060352e84c37d4122c
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2024-06-04 20:11:40 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,6 +1,7 @@
 function solution(emergency) {
     //배열 내림차순 -> 새 배열 생성
-    let bb = [...emergency].sort(function(a,b){
+    let copy = JSON.parse(JSON.stringify(emergency))
+    let bb = copy.sort(function(a,b){
       return b-a
     });
     
@@ -9,12 +10,6 @@ function solution(emergency) {
     bb.forEach((item,index)=>{
       result.push([item,index+1])
     });
-
-    /* result = [
-        [76,1],
-        [24,2],
-        [3,3]
-    ] */
 
     let test = [];
     emergency.forEach(item => {
@@ -27,7 +22,3 @@ function solution(emergency) {
     
     return test;
 }
-
-//객체, 배열은 참조값으로 참조값이 데이터 자체는 별도의 메모리 공간에 저장되고 변수에 할당 시 데이터 값이 아닌
-//데이터에 대한 메모리 공간의 주소가 저장됨!!! 참조값을 복사 하게 되면 두 변수는 같은 객체를 바라봄
-// => 참조값은 복사해서 사용할 때 얕은 복사해서 사용해야함 + 이중배열 만들기도 배웠다
