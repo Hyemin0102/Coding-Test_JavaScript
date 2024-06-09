31cc2aecbc8b5bc7f009210b293a1621324b1842
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2024-06-09 15:58:10 +0900

    [level 0] Title: 암호 해독, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,12 +1,14 @@
 function solution(cipher, code) {
-    var answer = "";
-
-    for(let i = 0; i < cipher.length; i++){
-      if((i+1) % code === 0){
-        answer += cipher[i]
-      }
+    let cipherArr = [...cipher]; //문자열 -> 배열로
+    let answer = [];
+    for(let i=0; i<cipherArr.length; i++){ //cipherArr의 0번째 아이템부터 순회
+        if((i+1) % code === 0){ //0 나누기 4는 0 -> i에 1 더해서 자리 찾기
+            answer.push(cipherArr[i])
+        }
     }
-    return answer;
-  }
 
-//cipher 문자열 길이만큼 순회 -> i자리+1 % code 나머지가 0 인 것 배열 합침
+    return answer.join("");
+}
+
+//code의 배수열 찾아서 그 값들만 새 배열로 출력
+//cipher 순회 -> code 나눠서 나머지 0인거 찾음 -> 0번째 인덱스는 무조건 0으로 나옴
