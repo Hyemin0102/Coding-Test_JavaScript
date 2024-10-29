e61a5e9257a24b4b45afe9327ffb18f0d33c8e6d
HyeMin <128768462+Hyemin0102@users.noreply.github.com>
2024-10-29 20:21:00 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,3 +1,8 @@
 function solution(my_string, k) {
-  return my_string.repeat(k)
+  //문자열을 k 만큼 반복
+    let result = '';
+    for(let i=0; i<k; i++){
+        result += my_string
+    }
+    return result;
 }
